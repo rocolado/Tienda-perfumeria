e1549a08483c1f7ee9d410009add257537107fa0
rocolado <r.colado.chaves@gmail.com>
2022-04-28 16:54:40 +0200

    Cambios en slider

--- Proyecto/src/index.js
+++ Proyecto/src/index.js
@@ -1,15 +1,29 @@
-// let btnCirculo1 = document.querySelector("#circulo1"),
-//     btnCirculo2 = document.querySelector("#circulo2"),
-//     btnCirculo3 = document.querySelector("#circulo3");
+/* primer slider */
+
+let btnCirculo1 = document.querySelector("#circulo1"),
+    btnCirculo2 = document.querySelector("#circulo2"),
+    btnCirculo3 = document.querySelector("#circulo3");
+
+const slidesPromos = document.querySelectorAll(".slidesPromos");
 
 const primerSlider = document.querySelectorAll(".primerSlider");
 const segundoSlider = document.querySelectorAll(".segundoSlider");
-const circuloSlider = document.querySelectorAll(".circuloSlider");
+// const circuloSlider = document.querySelectorAll(".circuloSlider");
 
 let slideIndex1 = 0;
 let slideIndex2 = 0;
 let circulosIndex = 0;
 
+
+/* slider marcas */
+
+let logos = document.querySelectorAll(".imgLogos");
+
+let prev = document.querySelector("#prev"),
+    next = document.querySelector("#next");
+
+
+/* función mostrar primer slider (versión móvil) */
 
 const mostrarPrimerSlider = () => {
     primerSlider[0].style.display = "none";
@@ -27,6 +41,9 @@ const mostrarPrimerSlider = () => {
     primerSlider[slideIndex1-1].style.display = "block";
     setTimeout(mostrarPrimerSlider, 2000);
 }
+
+
+/* función mostrar primer slider (versión ordenador) */
 
 const mostrarSegundoSlider = () => {
     segundoSlider[0].style.display = "none";
@@ -54,7 +71,8 @@ mostrarSegundoSlider();
 //     // console.log(circuloSlider);
 
 //     for (let i = 1; i < circuloSlider.length; i++) {
-//         circuloSlider[i].classList.replace("fas", "far");
+//         // circuloSlider[i].classList.replace("fas", "far");
+//         circuloSlider[i].classList.add("fas");
 //     }
 
 //     circulosIndex++;
@@ -63,30 +81,13 @@ mostrarSegundoSlider();
 //         circulosIndex = 1;
 //     }
 
-//     circuloSlider[circulosIndex-1].classList.replace("far", "fas");
+//     // circuloSlider[circulosIndex-1].classList.replace("far", "fas");
+//     circuloSlider[circulosIndex-1].classList.add("fas");
+//     // console.log(circuloSlider);
 //     setTimeout(cambioCirculos, 2000);
 // }
 
 // cambioCirculos();
-
-// const cambioCirculo = () => {
-//     let circuloSlider = document.querySelectorAll(".circuloSlider");
-
-//     for (let j = 0; j < circuloSlider.length; j++) {
-//         circuloSlider[j].classList.replace("far", "fas");
-//     }
-
-//     circulosIndex++;
-
-//     if (circulosIndex > circuloSlider.length) {
-//         circulosIndex = 1;
-//     }
-
-//     circuloSlider[circulosIndex-1].classList.replace("fas", "far");
-//     setTimeout(showSlides,2000);
-// }
-
-// cambioCirculo();
 
 
 // const cambioCirculos = () => {
@@ -96,49 +97,56 @@ mostrarSegundoSlider();
 //     btnCirculo3.classList.replace("fas", "far");
 
 //     // circulo slider2
-//     btnCirculo1.classList.replace("fas", "far");
-//     btnCirculo2.classList.replace("far", "fas");
-//     btnCirculo3.classList.replace("fas", "far");
+//     // btnCirculo1.classList.replace("fas", "far");
+//     // btnCirculo2.classList.replace("far", "fas");
+//     // btnCirculo3.classList.replace("fas", "far");
 // }
 
+/* cambio clase de círculos - primer slider */
 
-// btnCirculo1.addEventListener("click", () => {
-//     // slider1 tamaño grande y pequeño
-//     slidersPromos[0].style.display = "flex";
-//     slidersPromos[3].style.display = "flex"; 
+btnCirculo1.addEventListener("click", () => {
+    // slider1 tamaño grande y pequeño
+    slidesPromos[0].style.display = "flex";
+    slidesPromos[3].style.display = "flex"; 
 
-//     slidersPromos[1].style.display = "none";
-//     slidersPromos[2].style.display = "none";
-//     slidersPromos[4].style.display = "none";
-//     slidersPromos[5].style.display = "none";
+    slidesPromos[1].style.display = "none";
+    slidesPromos[2].style.display = "none";
+    slidesPromos[4].style.display = "none";
+    slidesPromos[5].style.display = "none";
 
-//     cambioCirculos();
-// });
+    btnCirculo1.classList.replace("far", "fas");
+    btnCirculo2.classList.replace("fas", "far");
+    btnCirculo3.classList.replace("fas", "far");
+});
 
 
-// btnCirculo2.addEventListener("click", () => {
-//     // slider2 tamaño grande y pequeño
-//     slidersPromos[1].style.display = "flex";
-//     slidersPromos[4].style.display = "flex";
+btnCirculo2.addEventListener("click", () => {
+    // slider2 tamaño grande y pequeño
+    slidesPromos[1].style.display = "flex";
+    slidesPromos[4].style.display = "flex";
     
-//     slidersPromos[0].style.display = "none";
-//     slidersPromos[2].style.display = "none";
-//     slidersPromos[3].style.display = "none";
-//     slidersPromos[5].style.display = "none";
-// });
+    slidesPromos[0].style.display = "none";
+    slidesPromos[2].style.display = "none";
+    slidesPromos[3].style.display = "none";
+    slidesPromos[5].style.display = "none";
+
+    btnCirculo1.classList.replace("fas", "far");
+    btnCirculo2.classList.replace("far", "fas");
+    btnCirculo3.classList.replace("fas", "far");
+});
 
 
-// btnCirculo3.addEventListener("click", () => {
-//     // slider3 tamaño grande y pequeño
-//     slidersPromos[2].style.display = "flex";
-//     slidersPromos[5].style.display = "flex";
+btnCirculo3.addEventListener("click", () => {
+    // slider3 tamaño grande y pequeño
+    slidesPromos[2].style.display = "flex";
+    slidesPromos[5].style.display = "flex";
 
-//     slidersPromos[0].style.display = "none";
-//     slidersPromos[1].style.display = "none";
-//     slidersPromos[3].style.display = "none";
-//     slidersPromos[4].style.display = "none";
+    slidesPromos[0].style.display = "none";
+    slidesPromos[1].style.display = "none";
+    slidesPromos[3].style.display = "none";
+    slidesPromos[4].style.display = "none";
 
-//     btnCirculo1.classList.replace("fas", "far");
-//     btnCirculo2.classList.replace("fas", "far");
-//     btnCirculo3.classList.replace("far", "fas");
-// });
+    btnCirculo1.classList.replace("fas", "far");
+    btnCirculo2.classList.replace("fas", "far");
+    btnCirculo3.classList.replace("far", "fas");
+});
